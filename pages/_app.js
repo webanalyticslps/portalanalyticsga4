ee7f1151006444b7661b381bf9cde9d4ab1c962f
Lopes <vsales@lopes.com.br>
2022-07-21 00:29:44 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import SSRProvider from 'react-bootstrap/SSRProvider';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-	<script src="http://localhost:8097" />;
+//	<script src="http://localhost:8097" />;
 	useEffect(() => {
 		import('bootstrap/dist/js/bootstrap');
 	}, []);
