bac6089f61d2ef80067b59cf5c3f7d9ac7020883
Lopes <vsales@lopes.com.br>
2022-07-21 00:44:56 -0300

    first commit

--- pages/documentacoes/como-solicitar-tagueamento.tsx
+++ pages/documentacoes/como-solicitar-tagueamento.tsx
@@ -38,7 +38,7 @@ const PaginaComoSolicitarTagueamento = () => {
 						<div className="accordion-body">
 							As solicitações de atendimento ao time de BI/Data Analytics, deverão ser feitas única e
 							exclusivamente pelo link de suporte dentro do sistema Lopesnet, através do ícone
-							"Relatórios", selecionando em seguida a subcategoria desejada. Obs: Em breve teremos duas
+							Relatórios, selecionando em seguida a subcategoria desejada. Obs: Em breve teremos duas
 							novas categorias dentro do ícone Relatórios: Metabase e Web Analytics. Enquanto essas
 							novas categorias não são implementadas, as solicitações para essas categorias, podem ser
 							enviadas para o e-mail acima citado.
