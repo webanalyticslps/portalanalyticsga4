fddd483848fadfa1f7bd48b58d4a238c4221ec58
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2022-07-22 13:58:18 -0300

    first commit

--- pages/parametros.tsx
+++ pages/parametros.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from 'next';
-import { getAllParametros, Metricas } from '../lib/db';
+import { getAllParametros, Parametros } from '../lib/db';
 import Head from 'next/head';
 import { Container, Row, Card, Button } from 'react-bootstrap';
 
