cd1bd13366cffcea9ba9d5fdbba504846e7825d4
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2022-07-28 14:09:39 -0300

    first commit

--- pages/eventos.tsx
+++ pages/eventos.tsx
@@ -1,46 +1,44 @@
-import { GetServerSideProps } from 'next';
-import { getAllEventos, Eventos } from '../lib/db';
-import Head from 'next/head';
-import { Container, Row, Card, Button } from 'react-bootstrap';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { GetServerSideProps } from "next";
+import { getAllEventos, Eventos } from "../lib/db";
+import Head from "next/head";
+import { Container, Row, Card, Button } from "react-bootstrap";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export const getServerSideProps: GetServerSideProps = async () => {
-	const eventos = await getAllEventos();
-	return {
-		props: {
-			eventos
-		}
-	};
+  const eventos = await getAllEventos();
+  return {
+    props: {
+      eventos,
+    },
+  };
 };
 
 interface PostProps {
-	eventos: Eventos[];
+  eventos: Eventos[];
 }
 
-export default withPageAuthRequired(function Profile({ user }) {
-const PáginaDeEventos = ({ eventos }: PostProps) => {
-	return (
-		<div className="container">
-			<h2>
-				<p className="text-center font-weight-bold">EVENTOS</p>
-			</h2>
-			<div>
-				<div className="row text-light bg-lopes border border-dark">
-					<div className="col-sm">ID</div>
-					<div className="col-sm">Nome</div>
-					<div className="col-sm">Escopo</div>
-					<div className="col-sm">Descrição</div>
-				</div>
-			</div>
-			{eventos.map((Eventos, index) => (
-				<div key={Eventos.id} className="row border border-dark">
-					<div className="col-sm">{Eventos.id}</div>
-					<div className="col-sm">{Eventos.nome}</div>
-					<div className="col-sm">{Eventos.escopo}</div>
-					<div className="col-sm">{Eventos.descricao}</div>
-				</div>
-			))}
-		</div>
-	);
-};
+export default withPageAuthRequired(function Profile({ eventos }) {
+  return (
+    <div className="container">
+      <h2>
+        <p className="text-center font-weight-bold">EVENTOS</p>
+      </h2>
+      <div>
+        <div className="row text-light bg-lopes border border-dark">
+          <div className="col-sm">ID</div>
+          <div className="col-sm">Nome</div>
+          <div className="col-sm">Escopo</div>
+          <div className="col-sm">Descrição</div>
+        </div>
+      </div>
+      {eventos.map((Eventos, index) => (
+        <div key={Eventos.id} className="row border border-dark">
+          <div className="col-sm">{Eventos.id}</div>
+          <div className="col-sm">{Eventos.nome}</div>
+          <div className="col-sm">{Eventos.escopo}</div>
+          <div className="col-sm">{Eventos.descricao}</div>
+        </div>
+      ))}
+    </div>
+  );
 });
