48abf3d5dc4be65c81b11cdd069aa7c22779c269
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2022-09-06 11:15:20 -0300

    first commit

--- pages/relatorios.tsx
+++ pages/relatorios.tsx
@@ -1,25 +1,60 @@
-import { GetServerSideProps } from 'next';
-import Head from 'next/head';
-import { Container, Row, Card, Button } from 'react-bootstrap';
-import Image from 'next/image';
-import react from 'react';
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { Container, Row, Card, Button } from "react-bootstrap";
+import Image from "next/image";
+import react from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withPageAuthRequired(function Profile({ user }) {
-	return (
-		<div className="container">
-			<div className="container">
-				<div className="row">
-					<div className="col">
-						<div className="card text-center">
-							[Digital Analytics] [UA] Dashboard Sherlopes Home
-							<Image src="/sherlopes.png" alt="" title="" width="300" height="400" layout="responsive" />
-							Relatório construído para acompanhar a jornada de geração de leads do produto Sherlopes Home.
-						</div>
-					</div>
-				</div>
-                
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card text-center">
+              [Digital Analytics] [UA] Dashboard Sherlopes Home
+              <Image
+                src="/sherlopes.png"
+                alt=""
+                title=""
+                width="300"
+                height="400"
+                layout="responsive"
+              />
+              Relatório construído para acompanhar a jornada de geração de leads
+              do produto Sherlopes Home.
+            </div>
+          </div>
+          <div className="col">
+            <div className="card text-center">
+              Vazio
+              <Image
+                src="/background.png"
+                alt=""
+                title=""
+                width="300"
+                height="400"
+                layout="responsive"
+              />
+              Vazio
+            </div>
+          </div>
+          <div className="col">
+            <div className="card text-center">
+              Vazio
+              <Image
+                src="/background.png"
+                alt=""
+                title=""
+                width="300"
+                height="400"
+                layout="responsive"
+              />
+              Vazio
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 });
