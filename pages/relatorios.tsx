8b3bd6da0950e76cdb9ae0ecd16ad573a52b8722
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2022-10-10 10:28:07 -0300

    first commit

--- pages/relatorios.tsx
+++ pages/relatorios.tsx
@@ -14,45 +14,47 @@ export default withPageAuthRequired(function Profile({ user }) {
             <div className="card text-center">
               [Digital Analytics] [UA] Dashboard Sherlopes Home
               <a href="https://datastudio.google.com/u/0/reporting/10e69f92-8ba0-441e-aa26-f0233d32b402/page/7BuKC">
-			  <Image
-                src="/sherlopes.png"
-                alt=""
-                title=""
-                width="300"
-                height="400"
-                layout="responsive"
-              />
-			  </a>
+                <Image
+                  src="/sherlopes.png"
+                  alt=""
+                  title=""
+                  width="300"
+                  height="400"
+                  layout="responsive"
+                />
+              </a>
               Relatório construído para acompanhar a jornada de geração de leads
               do produto Sherlopes Home.
             </div>
           </div>
           <div className="col">
             <div className="card text-center">
-            [Digital Analytics] [UA] Dashboard Produtos Recomendados
+              [Digital Analytics] [UA] Dashboard Produtos Recomendados
               <Image
-                src="/background.png"
+                src="/relatorios produtos recomendados.png"
                 alt=""
                 title=""
                 width="300"
                 height="400"
                 layout="responsive"
               />
-              Relatório construído para acompanhar os indicadores do produto de dados "Imóveis Recomendados".
+              Relatório construído para acompanhar os indicadores do produto de
+              dados "Imóveis Recomendados".
             </div>
           </div>
           <div className="col">
             <div className="card text-center">
-              Vazio
+              [Digital Analytics] [UA] Dashboard Produtos Similares
               <Image
-                src="/background.png"
+                src="/produtossimilares.png"
                 alt=""
                 title=""
                 width="300"
                 height="400"
                 layout="responsive"
               />
-              Vazio
+              Relatório construído para acompanhar os indicadores do produto de
+              dados "Imóveis Similares".
             </div>
           </div>
         </div>
