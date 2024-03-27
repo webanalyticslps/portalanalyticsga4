32eb1e30effab41d4d740aed249d13630919bea5
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2024-03-27 11:38:13 -0300

    first commit

--- pages/historico.tsx
+++ pages/historico.tsx
@@ -45,8 +45,6 @@ export default withPageAuthRequired(function Profile({
     console.log("Abrindo modal para:", historico); // Adicione isto para depuração
     setSelectedHistorico(historico);
     setShowModal(true);
-    setTipoRegistro(historico.tipo_registro || "");
-    setTipoImplementacao(historico.tipo_implementacao || "");
   };
 
   // Função para fechar o modal
@@ -141,22 +139,19 @@ export default withPageAuthRequired(function Profile({
             <Form.Control
               type="text"
               placeholder="Tipo de registro"
-              value={tipoRegistro}
-              onChange={(e) => setTipoRegistro(e.target.value)}
+              defaultValue={tipoRegistro}
             />
             <Form.Label>Tipo de implementação</Form.Label>
             <Form.Control
               type="text"
               placeholder="Tipo de implementação"
               defaultValue={selectedHistorico?.tipo_implementacao}
-              onChange={(e) => setTipoImplementacao(e.target.value)}
             />
             <Form.Label>Descrição</Form.Label>
             <Form.Control
               type="text"
               placeholder="Descrição"
               defaultValue={selectedHistorico?.descricao}
-              onChange={(e) => setDescricao(e.target.value)}
             />
             <Form.Label>Data e hora</Form.Label>
             <Form.Control
@@ -169,7 +164,6 @@ export default withPageAuthRequired(function Profile({
                       .slice(0, 16)
                   : ""
               }
-              onChange={(e) => setDataHora(e.target.value)}
             />
 
             <Form.Label>Status</Form.Label>
@@ -177,42 +171,36 @@ export default withPageAuthRequired(function Profile({
               type="text"
               placeholder="Status"
               defaultValue={selectedHistorico?.status}
-              onChange={(e) => setStatus(e.target.value)}
             />
             <Form.Label>Responsável</Form.Label>
             <Form.Control
               type="text"
               placeholder="Responsável"
               defaultValue={selectedHistorico?.responsavel}
-              onChange={(e) => setResponsavel(e.target.value)}
             />
             <Form.Label>Container ID GTM</Form.Label>
             <Form.Control
               type="text"
               placeholder="Container ID GTM"
               defaultValue={selectedHistorico?.container_id_gtm}
-              onChange={(e) => setContainerIdGtm(e.target.value)}
             />
             <Form.Label>Propriedade GA4</Form.Label>
             <Form.Control
               type="text"
               placeholder="Propriedade GA4"
               defaultValue={selectedHistorico?.propriedade_id_ga4}
-              onChange={(e) => setPropriedadeIdGa4(e.target.value)}
             />
             <Form.Label>Impacto</Form.Label>
             <Form.Control
               type="text"
               placeholder="Impacto"
               defaultValue={selectedHistorico?.impacto}
-              onChange={(e) => setImpacto(e.target.value)}
             />
             <Form.Label>Solução</Form.Label>
             <Form.Control
               type="text"
               placeholder="Solução"
               defaultValue={selectedHistorico?.solucao}
-              onChange={(e) => setSolucao(e.target.value)}
             />
             <Form.Label>Data e hora da resolução</Form.Label>
             <Form.Control
@@ -225,7 +213,6 @@ export default withPageAuthRequired(function Profile({
                       .slice(0, 16)
                   : ""
               }
-              onChange={(e) => setDataHoraResolucao(e.target.value)}
             />
           </Form.Group>
         </Form>
