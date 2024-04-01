4a91d8f04a9b72a00382705206f5d41ce84416ae
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2024-04-01 15:52:32 -0300

    first commit

--- pages/historico.tsx
+++ pages/historico.tsx
@@ -6,7 +6,7 @@ import {
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { EditModal } from "./editmodal";
+import { EditModal } from "/editmodal";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const historicoImplementacoesBugs = await getAllHistoricoImplementacoesBugs();
