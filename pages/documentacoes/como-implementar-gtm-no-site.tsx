5d0cb97b6772a82412dce96b49f5746b204655dc
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2024-04-12 17:12:31 -0300

    first commit

--- pages/documentacoes/como-implementar-gtm-no-site.tsx
+++ pages/documentacoes/como-implementar-gtm-no-site.tsx
@@ -184,6 +184,99 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           </div>
         </div>
       </div>
+
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingFour">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+            >
+              Como implementar GTM no site da Unilopes
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingFour"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>
+                Copie o código abaixo e cole-o em todas as páginas do seu
+                website. Cole esse código o mais alto possível na tag head da
+                página:
+              </p>
+
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows={9}
+                cols={50}
+                value="<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-W5XCPXHB');</script>"
+              />
+              <p />
+              <p>
+                Além disso, cole esse código imediatamente após a tag de
+                abertura body:
+              </p>
+
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows={4}
+                cols={50}
+                value='<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5XCPXHB"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingFour">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+            >
+              Como implementar GTM Server Side
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingFour"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>Copie o Container Config Abaixo no servidor:</p>
+
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows={9}
+                cols={50}
+                value="aWQ9R1RNLVRMQzdLNzUmZW52PTEmYXV0aD1KUmxFdmtuY1hiYklkWUhLalJJd09B"
+              />
+              <p />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 });
