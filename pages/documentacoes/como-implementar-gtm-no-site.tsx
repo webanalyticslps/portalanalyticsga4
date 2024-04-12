02a27169a041171addd6d32979b8b7f32c789fe3
Lopes <lopes@MacBook-Air-de-Administrador-4.local>
2024-04-12 17:02:54 -0300

    first commit

--- pages/documentacoes/como-implementar-gtm-no-site.tsx
+++ pages/documentacoes/como-implementar-gtm-no-site.tsx
@@ -24,7 +24,7 @@ export default withPageAuthRequired(function Profile({ user }) {
               aria-expanded="false"
               aria-controls="collapseOne"
             >
-              Como implementar GTM no ambiente de desenvolvimento
+              Como implementar GTM com as tags do GA4 no ambiente de desenvolvimento
             </button>
           </h2>
           <div
@@ -81,7 +81,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               aria-expanded="false"
               aria-controls="collapseTwo"
             >
-              Como implementar GTM no ambiente de produção
+              Como implementar GTM com as tags do GA4 no ambiente de produção
             </button>
           </h2>
           <div
@@ -126,6 +126,64 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           </div>
         </div>
       </div>
+
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              Como implementar GTM com as tags de mídia/CRM
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>
+                Copie o código abaixo e cole-o em todas as páginas do seu
+                website. Cole esse código o mais alto possível na tag head da
+                página:
+              </p>
+
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows={9}
+                cols={50}
+                value="<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NW5CWVM');</script>"
+              />
+              <p />
+              <p>
+                Além disso, cole esse código imediatamente após a tag de
+                abertura body:
+              </p>
+
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows={4}
+                cols={50}
+                value='<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NW5CWVM"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 });
